89a8412f4f05eaaeb14556e24a7d1f8e23fc561e
Ashish Bhatt <ashishp.bhatt@einfochips.com>
2017-04-25 19:06:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var sum = require('./modules/sum.js');
+var sub = require('./modules/sub.js');
+var mul = require('./modules/mul.js');
+var div = require('./modules/div.js');
+
+console.log("hello Devanshu How R u ?")
+var total = sum.sum(30,10);
+var total1 = sub.sub(20,20);
+var total2 = mul.mul(30,40);
+var total3 = div.div(50,10);
+
+console.log(total);
+console.log(total1);
+console.log(total2);
+console.log(total3);
