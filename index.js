b87e3b99eb40e34a17165bf70835941c57866cf4
Ashish Bhatt <ashishp.bhatt@einfochips.com>
2017-04-26 10:29:40 +0530

    MODIFIED : Changes in index.js

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ var sub = require('./modules/sub.js');
 var mul = require('./modules/mul.js');
 var div = require('./modules/div.js');
 
-console.log("hello Devanshu How R u ?")
 var total = sum.sum(30,10);
-var total1 = sub.sub(20,20);
+var total1 = sub.sub(50,20);
 var total2 = mul.mul(30,40);
 var total3 = div.div(50,10);
 
-console.log(total);
-console.log(total1);
-console.log(total2);
-console.log(total3);
+var sumdiv,subdiv,muldiv,ddiv;
+
+
+sumdiv = document.createElement('div');
+subdiv = document.createElement('div');
+muldiv = document.createElement('div');
+ddiv = document.createElement('div');
+
+sumdiv.innerHTML = "SUMATION : " + total;
+subdiv.innerHTML = "SUBTRATION : " + total1;
+muldiv.innerHTML = "MULTIPLICATION : " + total2;
+ddiv.innerHTML = "DIVISION : " + total3;
+
+
+document.body.appendChild(sumdiv);
+document.body.appendChild(subdiv);
+document.body.appendChild(muldiv);
+document.body.appendChild(ddiv);
